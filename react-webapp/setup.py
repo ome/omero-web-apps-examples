05462c3f6c1099d34b871a6f67c7a72e272b66b1
William Moore <w.moore@dundee.ac.uk>
2019-11-27 13:56:34 +0000

    Add setup.py for react_webapp

---
+++ react-webapp/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='react-webapp',
+    version='0.0.1',
+    description="OMERO example React web app",
+    packages=find_packages(),
+    keywords=['omero', 'example', 'react'],
+)
